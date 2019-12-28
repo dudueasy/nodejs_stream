3e6e75664e64cf7c8d600cc0d1338385da81b6ac
dudueasy <dudueasy@126.com>
2019-12-28 21:13:42 +0800

    feat: update transform stream example

--- transform_stream_practices/file_compressor.js
+++ transform_stream_practices/file_compressor.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
 const zlib = require('zlib');
+const {Transform} = require('stream');
 
+const reportProgress = new Transform({
+  transform(chunk, encoding, callback) {
+    process.stdout.write('.');
+    callback(null, chunk);
+  },
+});
 
 function file_compressor(fileName, outputPath) {
   fs.createReadStream(fileName)
+    .pipe(reportProgress)
     .pipe(zlib.createGzip())
-    .pipe(fs.createWriteStream(`${ outputPath }.gz`));
+    .pipe(fs.createWriteStream(`${outputPath}.gz`));
 }
+
 
 // module.exports = file_compressor;
 
-const fileName = process.argv[2]
-file_compressor(fileName, fileName)
+const fileName = process.argv[2];
+file_compressor(fileName, fileName);
